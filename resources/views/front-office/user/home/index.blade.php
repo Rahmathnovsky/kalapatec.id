2a9b0df5b1b7ba51e176f9b16967234064505969
celzadinata <dinatacelza@gmail.com>
2025-09-24 18:45:50 +0700

    fix: UI

--- resources/views/front-office/user/home/index.blade.php
+++ resources/views/front-office/user/home/index.blade.php
@@ -26,7 +26,7 @@
                 >
                   <li>
                     <a
-                      href="javascript:void(0)"
+                      href="#services"
                       class="inline-flex items-center justify-center rounded-md bg-primary-color text-primary px-5 py-3 text-center text-base font-medium shadow-md hover:bg-primary-light-5 md:px-7 md:py-[14px]"
                       role="button"
                       >@lang('home.get_started')</a
